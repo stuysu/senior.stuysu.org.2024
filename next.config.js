fc8673349e16ab449e46ea7ee615801df6cd9fae
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-09-12 01:42:26 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,9 @@
-const isProd = process.env.NODE_ENV === "production";
+const debug = process.env.NODE_ENV !== "production";
 
 module.exports = {
   reactStrictMode: true,
   basePath: process.env.NEXT_PUBLIC_BASE_PATH,
-  assetPrefix: isProd ? '/stuyjucoc-frontend' : '',
+  assetPrefix: !debug ? 'https://pserb.github.io/stuyjucoc-frontend/' : '',
   images: {
     loader: "imgix",
     path: "",
