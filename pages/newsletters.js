65a1e7754880064889e847dac632b62f3e46b145
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-09-12 16:35:56 -0400

    added newsletters, opportunities, about page

--- pages/newsletters.js
+++ pages/newsletters.js
@@ -1,17 +1,15 @@
 import Head from 'next/head'
 // import Image from 'next/image'
-import home from '../styles/Home.module.scss'
+import newsletters from '../styles/Newsletters.module.scss'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
-// const prefix = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
-export default function Home() {
-  console.log(process.env.NEXT_PUBLIC_BASE_PATH)
-  return (
-    <div className={home.container}>
+export default function Newsletters() {
+    console.log(process.env.NEXT_PUBLIC_BASE_PATH)
+    return (
+    <div className="container">
       <Head>
-        <title>Junior Caucus</title>
+        <title>Newsletters | Junior Caucus</title>
         <meta charSet="UTF-8" />
         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -19,16 +17,13 @@ export default function Home() {
         <link rel="shortcut icon" href="./static/JuniorCaucusWhite.png" />
       </Head>
 
-      <div className={home.content}>
+      {/* <div className={home.content}> */}
         <Navbar />
 
-        <section className={home.cardcontainer}>
-          <img className={home.cardstuyimg} src="./static/stuy.jpg"/>
-          <img className={home.cardlogo} src="./static/JuniorCaucusWhite.png"/>
-        </section>
+        <h1 className={"text-center" + " " + newsletters.title}>Newsletters</h1>
 
         <Footer />
-      </div>
+      {/* </div> */}
     </div>
-  )
+    )
 }
