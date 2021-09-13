0536865f4f05be4ed312cce591a876ed7be9b965
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-09-12 20:28:30 -0400

    trying mobile nav -- is broken

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,10 +2,19 @@ import Link from 'next/link'
 
 export default function Navbar({ }) {
     return <nav className="main-nav">
-    <Link href="/"><a>Home</a></Link>
-    <Link href="/newsletters"><a>Newsletters</a></Link>
-    <Link href="/opportunities"><a>Opportunities</a></Link>
-    <Link href="/about"><a>About</a></Link>
+    <input type="checkbox" className="input" />
+      <div className="mobile-menu-toggle">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    <Link className="main-link" href="/"><a>Home</a></Link>
+    <Link className="main-link" href="/newsletters"><a>Newsletters</a></Link>
+    <Link className="main-link" href="/opportunities"><a>Opportunities</a></Link>
+    <Link className="main-link" href="/about"><a>About</a></Link>
     <a>Logo Logo Logo</a>
+    {/* <a className="mobile-nav-burger">
+      <i class="fa fa-bars"></i>
+    </a> */}
   </nav>;
 }
--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="Stuyvesant High School Junior Caucus" />
         <link rel="shortcut icon" href="./static/JuniorCaucusWhite.png" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
       </Head>
 
       <div className={home.content}>
