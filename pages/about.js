b6bc055826983e1f491bf661da1881918778510e
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-09-15 02:05:17 -0400

    cleaned up page components

--- pages/about.js
+++ pages/about.js
@@ -1,29 +1,17 @@
-import Head from 'next/head'
-// import Image from 'next/image'
 import about from '../styles/About.module.scss'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
-export default function Newsletters() {
-    console.log(process.env.NEXT_PUBLIC_BASE_PATH)
-    return (
+const About = () => {
+  return (
     <div className="container">
-      <Head>
-        <title>Newsletters | Junior Caucus</title>
-        <meta charSet="UTF-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta name="description" content="Stuyvesant High School Junior Caucus" />
-        <link rel="shortcut icon" href="./static/JuniorCaucusWhite.png" />
-      </Head>
-
-      {/* <div className={home.content}> */}
         <Navbar />
 
         <h1 className={"text-center" + " " + about.title}>About</h1>
 
         <Footer />
-      {/* </div> */}
     </div>
-    )
+  )
 }
+
+export default About;
