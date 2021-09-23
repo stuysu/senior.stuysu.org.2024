b75ab19c73e7d45a981ec42a758ca77abe29d50e
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-09-23 01:33:15 -0400

    Configured mobile nav to close when a link is clicked

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,12 @@
 import Link from 'next/link'
+import { useState } from 'react'
 import navbar from '../styles/Navbar.module.scss'
 
 export default function Navbar({ }) {
+  const [ checked, setChecked ] = useState(false);
+
+  function toggle() { setChecked(!checked) }
+
     return <>
     <nav className={navbar.mainnav}>
       <Link className="main-link" href="/"><a>Home</a></Link>
@@ -17,15 +22,15 @@ export default function Navbar({ }) {
 
     <nav className={navbar.mobilenav} role="navigation">
       <div id={navbar.menuToggle}>
-          <input type="checkbox" />
+        <input type="checkbox" checked={checked} onChange={toggle} />
           <span></span>
           <span></span>
           <span></span>
           <ul id={navbar.menu}>
-            <Link className="main-link" passHref href="/"><li><a>Home</a></li></Link>
-            <Link className="main-link" passHref href="/newsletters"><li><a>Newsletters</a></li></Link>
-            <Link className="main-link" passHref href="/opportunities"><li><a>Opportunities</a></li></Link>
-            <Link className="main-link" passHref href="/about"><li><a>About</a></li></Link>
+            <Link className="main-link" passHref href="/"><li><a onClick={toggle}>Home</a></li></Link>
+            <Link className="main-link" passHref href="/newsletters"><li><a onClick={toggle}>Newsletters</a></li></Link>
+            <Link className="main-link" passHref href="/opportunities"><li><a onClick={toggle}>Opportunities</a></li></Link>
+            <Link className="main-link" passHref href="/about"><li><a onClick={toggle}>About</a></li></Link>
             <li className={navbar.socials}>
               <a href="https://www.instagram.com/" target="_blank" rel="noreferrer"><i className="fa fa-instagram" aria-hidden="true"></i></a>
               <a href="https://www.facebook.com/" target="_blank" rel="noreferrer"><i className="fa fa-facebook" aria-hidden="true"></i></a>
@@ -33,10 +38,6 @@ export default function Navbar({ }) {
               </li>
           </ul>
       </div>
-      {/* <div className="nav-title-m" id="nav-m">
-          <Link href={"/"}><div className="link-txt" id="special">StuyMUN</div></Link>
-          <Link href={"/"}>{FilledImage}</Link>
-      </div> */}
     </nav>
     </>;
 }
