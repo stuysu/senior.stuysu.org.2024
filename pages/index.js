24b2b7d54275a8f5f6363969b4bc2f2a60365362
Paul Serbanescu <61565464+pserb@users.noreply.github.com>
2021-09-23 01:33:33 -0400

    added 3 color blocking prototypes and style

--- pages/index.js
+++ pages/index.js
@@ -14,6 +14,27 @@ export default function Home() {
         <img className={home.cardstuyimg} src={getImg("/stuy.jpg")} />
         <img className={home.cardlogo} src={getImg("/JuniorCaucusWhite.png")} />
       </section>
+
+      <section className={home.contentcontainer1}>
+        <div className={home.content}>
+          <h1 className="text-center title">Junior Caucus</h1>
+          <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed suscipit orci, at auctor massa. Sed cursus diam at tortor dignissim, ac blandit velit luctus. Suspendisse gravida enim nec enim sollicitudin sollicitudin. Proin vitae maximus risus, quis eleifend massa. Sed a nulla in nisl efficitur varius vel non sapien. Ut faucibus nunc ac quam cursus ultricies. Cras ornare quam et mauris pellentesque ultrices.</p>
+        </div>
+      </section>
+
+      <section className={home.contentcontainer2}>
+        <div className={home.content}>
+          <h1 className="text-center title">Junior Caucus</h1>
+          <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed suscipit orci, at auctor massa. Sed cursus diam at tortor dignissim, ac blandit velit luctus. Suspendisse gravida enim nec enim sollicitudin sollicitudin. Proin vitae maximus risus, quis eleifend massa. Sed a nulla in nisl efficitur varius vel non sapien. Ut faucibus nunc ac quam cursus ultricies. Cras ornare quam et mauris pellentesque ultrices.</p>
+        </div>
+      </section>
+
+      <section className={home.contentcontainer3}>
+        <div className={home.content}>
+          <h1 className="text-center title">Junior Caucus</h1>
+          <p className="text-center">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis sed suscipit orci, at auctor massa. Sed cursus diam at tortor dignissim, ac blandit velit luctus. Suspendisse gravida enim nec enim sollicitudin sollicitudin. Proin vitae maximus risus, quis eleifend massa. Sed a nulla in nisl efficitur varius vel non sapien. Ut faucibus nunc ac quam cursus ultricies. Cras ornare quam et mauris pellentesque ultrices.</p>
+        </div>
+      </section>
     </>
   );
 }
