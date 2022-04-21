b2a4094f92661a9c0eda537a76d144f2bca7c116
Kevin Wang <kwang30@stuy.edu>
2022-04-21 19:23:02 -0400

    initalized design for about page with Andreys, need to be populated in with information

--- pages/about.js
+++ pages/about.js
@@ -1,8 +1,7 @@
 import Head from "next/head";
-
+import Profile from "../components/Profile";
 import about from "../styles/About.module.scss";
-import Footer from "../components/Footer";
-import Navbar from "../components/Navbar";
+
 
 export default function About() {
   return (
@@ -12,6 +11,163 @@ export default function About() {
       </Head>
 
       <h1 className="text-center title">About</h1>
+
+      <div className={about.department}>
+        
+        <h1>Executives</h1>
+        <hr></hr>
+        <div className={about.profilesContainer}>
+
+        <Profile
+        name="Andrey Sokolov"
+        imagePath="../Default_pfp.jpeg"
+        facebook="https://www.facebook.com/profile.php?id=100037622327482"
+        instagram="FILL HERE"
+        github="FILL HERE"
+        email="FILL HERE"
+        >
+        </Profile>
+
+        <Profile
+        name="Andrey Sokolov"
+        imagePath="../Default_pfp.jpeg"
+        facebook="https://www.facebook.com/profile.php?id=100037622327482"
+        instagram="FILL HERE"
+        github="FILL HERE"
+        email="FILL HERE"
+        >
+        </Profile>
+        
+
+        <Profile
+        name="Andrey Sokolov"
+        imagePath="../Default_pfp.jpeg"
+        facebook="https://www.facebook.com/profile.php?id=100037622327482"
+        instagram="FILL HERE"
+        github="FILL HERE"
+        email="FILL HERE"
+        >
+        </Profile>
+
+        <Profile
+        name="Andrey Sokolov"
+        imagePath="../Default_pfp.jpeg"
+        facebook="https://www.facebook.com/profile.php?id=100037622327482"
+        instagram="FILL HERE"
+        github="FILL HERE"
+        email="FILL HERE"
+        >
+        </Profile>
+
+        <Profile
+        name="Andrey Sokolov"
+        imagePath="../Default_pfp.jpeg"
+        facebook="https://www.facebook.com/profile.php?id=100037622327482"
+        instagram="FILL HERE"
+        github="FILL HERE"
+        email="FILL HERE"
+        >
+        </Profile>
+        </div>
+
+      </div>
+
+
+
+      <div className={about.department}>
+        
+        <h1>Executives</h1>
+        <hr></hr>
+        <div className={about.profilesContainer}>
+
+
+        <Profile
+        name="Andrey Sokolov"
+        imagePath="../Default_pfp.jpeg"
+        facebook="https://www.facebook.com/profile.php?id=100037622327482"
+        instagram="FILL HERE"
+        github="FILL HERE"
+        email="FILL HERE"
+        >
+        </Profile>
+        </div>
+
+      </div>
+
+
+
+      <div className={about.department}>
+        
+        <h1>Executives</h1>
+        <hr></hr>
+        <div className={about.profilesContainer}>
+
+        <Profile
+        name="Andrey Sokolov"
+        imagePath="../Default_pfp.jpeg"
+        facebook="https://www.facebook.com/profile.php?id=100037622327482"
+        instagram="FILL HERE"
+        github="FILL HERE"
+        email="FILL HERE"
+        >
+        </Profile>
+
+        <Profile
+        name="Andrey Sokolov"
+        imagePath="../Default_pfp.jpeg"
+        facebook="https://www.facebook.com/profile.php?id=100037622327482"
+        instagram="FILL HERE"
+        github="FILL HERE"
+        email="FILL HERE"
+        >
+        </Profile>
+
+
+        </div>
+
+      </div>
+
+
+
+      <div className={about.department}>
+        
+        <h1>Executives</h1>
+        <hr></hr>
+        <div className={about.profilesContainer}>
+
+        <Profile
+        name="Andrey Sokolov"
+        imagePath="../Default_pfp.jpeg"
+        facebook="https://www.facebook.com/profile.php?id=100037622327482"
+        instagram="FILL HERE"
+        github="FILL HERE"
+        email="FILL HERE"
+        >
+        </Profile>
+
+        <Profile
+        name="Andrey Sokolov"
+        imagePath="../Default_pfp.jpeg"
+        facebook="https://www.facebook.com/profile.php?id=100037622327482"
+        instagram="FILL HERE"
+        github="FILL HERE"
+        email="FILL HERE"
+        >
+        </Profile>
+
+        <Profile
+        name="Andrey Sokolov"
+        imagePath="../Default_pfp.jpeg"
+        facebook="https://www.facebook.com/profile.php?id=100037622327482"
+        instagram="FILL HERE"
+        github="FILL HERE"
+        email="FILL HERE"
+        >
+        </Profile>
+
+        </div>
+
+      </div>
     </>
   );
 }
