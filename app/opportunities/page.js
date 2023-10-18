913459a8156f76883759dd95fce5884f18b8f37f
Richard Wan <MyKnaifu@gmail.com>
2023-10-18 19:10:49 -0400

    fixed background image and text resizing on mobile

--- app/opportunities/page.js
+++ app/opportunities/page.js
@@ -1,43 +1,43 @@
-import scholarships from "@/data/scholarships.json";
-import HorizontalRuling from "@/components/HorizontalRuling";
+import scholarships from '@/data/scholarships.json'
+import HorizontalRuling from '@/components/HorizontalRuling'
 
 function getScholarships() {
-  let arr = [];
+  let arr = []
 
   for (let i = 0; i < scholarships.length; i += 2) {
     arr.push({
       title: scholarships[i],
       desc: scholarships[i + 1],
-    });
+    })
   }
 
-  return arr;
+  return arr
 }
 export default function Opportunities() {
-  let opportunities = getScholarships();
+  let opportunities = getScholarships()
 
   return (
-    <div className="pt-[50px]">
+    <div className='pt-[50px]'>
       {opportunities.map((opp, i) => (
-        <div className="pb-[60px]" key={i}>
+        <div className='pb-[60px]' key={i}>
           <HorizontalRuling>
-            <div className="relative w-[544px] h-[45px]">
-              <img
-                src="/about_us_background.svg"
-                className="absolute drop-shadow-lg w-[544px] h-[45px]"
-              />
-
-              <p className="absolute left-[20px] font-link font-bold text-[25px] text-[#45379D] underline top-[22.5px] -translate-y-1/2">
+            <div
+              className='relative w-3/5 md:w-2/5 h-auto bg-cover flex justify-center items-center overflow-auto'
+              style={{
+                backgroundImage: `url(${'/about_us_background.svg'})`,
+              }}
+            >
+              <p className='font-link font-bold lg:text-2xl sm:text-xl text-lg text-[#45379D] underline text-center p-2'>
                 {opp.title}
               </p>
             </div>
           </HorizontalRuling>
 
-          <p className="whitespace-pre-line text-[15px] text-[#45379D] px-[50px] pt-[40px]">
+          <p className='whitespace-pre-line text-[15px] text-[#45379D] px-[50px] pt-[40px]'>
             {opp.desc}
           </p>
         </div>
       ))}
     </div>
-  );
+  )
 }
