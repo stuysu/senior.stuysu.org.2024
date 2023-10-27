c5f39994da567d1207e568ad7eb26c8ec5fd4386
Richard Wan <MyKnaifu@gmail.com>
2023-10-27 18:54:06 -0400

    team card design update

--- app/team/MemberCard.js
+++ app/team/MemberCard.js
@@ -1,16 +1,16 @@
 export default function MemberCard({ name, title, image, bio }) {
   return (
-    <div className="relative rounded-lg w-[310px] bg-[#FDF4F2] lg:mb-0 mb-[25px] text-[#45379D]">
-      <div className="w-[310px] h-[310px] overflow-hidden">
-        <img className="rounded-t-lg object-cover" src={image} />
+    <div className='relative rounded-lg w-[310px] bg-[#FDF4F2] lg:mb-0 mb-[25px] text-[#45379D]'>
+      <div className='w-[310px] h-[310px] overflow-hidden'>
+        <img className='rounded-t-lg object-cover' src={image} />
       </div>
 
-      <div className="group min-h-[60px]">
-        <div className="mx-2">
+      <div className='group min-h-[60px]'>
+        <div className='mx-2'>
           <p
             data={name}
-            newData={bio}
-            className="font-bold underline group-hover:no-underline text-[20px] before:content-[attr(data)] group-hover:before:content-[attr(newData)]"
+            bio={bio}
+            className='font-bold underline group-hover:no-underline text-[20px] before:content-[attr(data)] group-hover:before:content-[attr(bio)]'
           ></p>
           <p
             data={title}
@@ -19,5 +19,5 @@ export default function MemberCard({ name, title, image, bio }) {
         </div>
       </div>
     </div>
-  );
+  )
 }
