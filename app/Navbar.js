69612eb425599577f12849611240471d0f0cd2d3
SJS830 <simonjs830@gmail.com>
2023-12-03 20:31:22 -0500

    start work on course selections

--- app/Navbar.js
+++ app/Navbar.js
@@ -48,6 +48,7 @@ export default function Navbar() {
         <a href="/">Home</a>
         <a href="/team">Team</a>
         <a href="/opportunities">Opportunities</a>
+        <a href="https://docs.google.com/document/d/1ltKbFo0SDqg417J16mmhc8D6DiNpYxOr6op9mmxx0aA/edit?usp=sharing">Course Selection Guide</a>
       </div>
 
       {/*hamburger*/}
@@ -102,6 +103,10 @@ export default function Navbar() {
 
         <a href="/opportunities" className="absolute top-[calc(48px+16vw)]">
           <div className="absolute top-[4vw] text-[#FFFFFF]">Opportunities</div>
+        </a>
+
+        <a href="https://docs.google.com/document/d/1ltKbFo0SDqg417J16mmhc8D6DiNpYxOr6op9mmxx0aA/edit?usp=sharing" className="absolute top-[calc(48px+24vw)]">
+          <div className="absolute top-[4vw] text-[#FFFFFF]">Course Selection Guide</div>
         </a>
       </div>
 
---
+++ app/courses/page.js
@@ -0,0 +1,12 @@
+export default function Courses() {
+  return (
+    <div className="relative bg-primary">
+      <p className="font-link absolute lg:text-[40px] text-[30px] font-bold lg:top-[90px] top-[40px] -translate-y-1/2 left-[50vw] -translate-x-1/2 text-text underline">
+        Course Selection Guide
+      </p>
+
+      <div className="lg:pt-[200px] pt-[100px]">
+      </div>
+    </div>
+  );
+}
