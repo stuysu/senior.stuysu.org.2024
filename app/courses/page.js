7c360d44cc4d798b0b13abecfc67408a3f246ea2
SJS830 <simonjs830@gmail.com>
2023-12-03 20:35:17 -0500

    /courses redirects

--- app/courses/page.js
+++ app/courses/page.js
@@ -1,5 +1,7 @@
+import { redirect } from 'next/navigation';
+
 export default function Courses() {
-  return (
+  /*return (
     <div className="relative bg-primary">
       <p className="font-link absolute lg:text-[40px] text-[30px] font-bold lg:top-[90px] top-[40px] -translate-y-1/2 left-[50vw] -translate-x-1/2 text-text underline">
         Course Selection Guide
@@ -8,5 +10,7 @@ export default function Courses() {
       <div className="lg:pt-[200px] pt-[100px]">
       </div>
     </div>
-  );
+  );*/
+
+  redirect("https://docs.google.com/document/d/1ltKbFo0SDqg417J16mmhc8D6DiNpYxOr6op9mmxx0aA/edit?usp=sharing");
 }
